b065f0c8fa8ea5079ee7a79fa15129d331f4643a
Edward Abraham <edward@dragonfly.co.nz>
2012-02-29 12:24:54 +1300

    Trying to get setup to work as expected

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(name              = 'django-centralauth',
         version         = '0.1',
         description     = 'Django support for a seperate authentication database',
         url             = 'https://github.com/dragonfly-science/django-authrouter.git',
-        packages        = ['centralauth'],
+        packages        = ['centralauth', 'centralauth.centralauth', 'centralauth.authrouter']
 )
 
