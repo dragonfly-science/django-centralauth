d65910cf85ff0df6fe6e0e5437a6c59b0e9aa617
Edward Abraham <edward@dragonfly.co.nz>
2012-02-29 12:10:21 +1300

    Typo

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ setup(name          = 'djano-centralauth',
         version         = '0.1',
         description     = 'Django support for a seperate authentication database',
         url             = 'https://github.com/dragonfly-science/django-authrouter.git',
-        patckages       = ['centralauth'],
+        packages       = ['centralauth'],
 )
 
