cb5766efde72c622cfa0d4fdd7fe672cea93a743
Edward Abraham <edward@dragonfly.co.nz>
2012-02-29 12:11:16 +1300

    Typing like a drunken monkey

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name          = 'djano-centralauth',
+setup(name              = 'django-centralauth',
         version         = '0.1',
         description     = 'Django support for a seperate authentication database',
         url             = 'https://github.com/dragonfly-science/django-authrouter.git',
-        packages       = ['centralauth'],
+        packages        = ['centralauth'],
 )
 
